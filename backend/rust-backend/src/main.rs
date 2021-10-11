38e080a512472882eb9dcbb02049bd57f77d0ce5
Juri <juri.lozowoj@freenet.de>
2021-10-11 15:46:09 +0200

    wip rust rest

--- backend/rust-backend/src/main.rs
+++ backend/rust-backend/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+mod route;
+
+
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(welcome))
+            .route("/{name}", web::get().to(welcome))
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
+
+async fn welcome(request: HttpRequest) -> impl Responder {
+    let name = request
+        .match_info()
+        .get("name")
+        .unwrap_or("World");
+    format!("Hello {}!", &name)
 }
